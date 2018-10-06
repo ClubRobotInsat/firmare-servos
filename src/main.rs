a457950e0ca9b6ec24c0092e682a059fa8482e85
Paul Florence <perso@florencepaul.com>
2018-10-06 08:36:39 +0200

    Fixed all references to blue pill to black pill.

--- src/main.rs
+++ src/main.rs
@@ -19,16 +19,16 @@ use core::fmt::Write; //  Provides writeln() function for debug console output.
 use cortex_m_rt::ExceptionFrame; //  Stack frame for exception handling.
 use cortex_m_semihosting::hio; //  For displaying messages on the debug console. //  Clocks, flash memory, GPIO for the STM32 Blue Pill.
 
-//  Blue Pill starts execution at function main().
+//  Black Pill starts execution at function main().
 entry!(main);
 
-//  Blue Pill starts execution here. "-> !" means this function will never return (because of the loop).
+//  Black Pill starts execution here. "-> !" means this function will never return (because of the loop).
 fn main() -> ! {
     //  Show "Hello, world!" on the debug console, which is shown in OpenOCD. "mut" means that this object is mutable, i.e. it can change.
     let mut debug_out = hio::hstdout().unwrap();
     //writeln!(debug_out, "Hello, world!").unwrap();
 
-    //  Get peripherals (clocks, flash memory, GPIO) for the STM32 Blue Pill microcontroller.
+    //  Get peripherals (clocks, flash memory, GPIO) for the STM32 Black Pill microcontroller.
     let bluepill = Peripherals::take().unwrap();
 
     //  Get the clocks from the STM32 Reset and Clock Control (RCC) and freeze the Flash Access Control Register (ACR).
@@ -39,7 +39,7 @@ fn main() -> ! {
     //  Get GPIO Port C, which also enables the Advanced Peripheral Bus 2 (APB2) clock for Port C.
     let mut gpioc = bluepill.GPIOC.split(&mut rcc.apb2);
 
-    //  Use Pin PC 13 of the Blue Pill for GPIO Port C. Select Output Push/Pull mode for the pin, which is connected to our LED.
+    //  Use Pin PC 13 of the Black Pill for GPIO Port C. Select Output Push/Pull mode for the pin, which is connected to our LED.
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
 
     //  Create a delay timer from the RCC clocks.
