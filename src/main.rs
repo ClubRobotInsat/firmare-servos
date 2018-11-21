fb6c22735ec636998730a7499e788f180c45c7b1
Paul Florence <perso@florencepaul.com>
2018-11-21 19:17:46 +0100

    First commit.

---
+++ src/main.rs
@@ -0,0 +1,145 @@
+#![no_main] //  Don't use the Rust standard bootstrap. We will provide our own.
+#![no_std] //  Don't use the Rust standard library. We are building a binary that can run on its own.
+
+extern crate cortex_m; //  Low-level functions for ARM Cortex-M3 processor in STM32 Blue Pill.
+#[macro_use(entry, exception)] //  Import macros from the following crates,
+extern crate cortex_m_rt; //  Startup and runtime functions for ARM Cortex-M3.
+extern crate arrayvec;
+extern crate cortex_m_semihosting; //  Debug console functions for ARM Cortex-M3.
+extern crate drs_0x01;
+extern crate embedded_hal;
+extern crate librobot;
+#[macro_use]
+extern crate nb;
+extern crate numtoa;
+extern crate panic_semihosting; //  Panic reporting functions, which transmit to the debug console.
+extern crate stm32f103xx;
+extern crate stm32f103xx_hal as f103_hal; //  Hardware Abstraction Layer (HAL) for STM32 Blue Pill.
+extern crate w5500;
+
+mod robot;
+
+// ------ Cortex | F103 imports
+use cortex_m::asm;
+use cortex_m::Peripherals as CortexPeripherals;
+use cortex_m_rt::ExceptionFrame;
+use f103::Peripherals;
+use f103_hal::delay::Delay;
+use f103_hal::prelude::*;
+use f103_hal::stm32f103xx as f103;
+
+// ------ Embedded HAL imports
+use embedded_hal::serial::Write as EWrite;
+use embedded_hal::spi::FullDuplex;
+
+// ------ Library imports
+use w5500::*;
+
+use drs_0x01::addr::WritableRamAddr;
+use drs_0x01::Servo as HServo;
+
+use librobot::transmission::servo::{Control, ServoGroup};
+use librobot::transmission::{Frame, Message, MessageKind};
+
+// ------ Local imports
+use robot::init_peripherals;
+
+const SOCKET_UDP: Socket = Socket::Socket0;
+
+entry!(main);
+
+fn init_servos(connection: &mut impl EWrite<u8>, delay: &mut Delay) {
+    let servo = HServo::new(0xFE);
+    let message = servo.reboot();
+    for b in message {
+        let _ = block!(connection.write(b));
+    }
+    delay.delay_ms(250u8);
+    let message = servo.enable_torque();
+    for b in message {
+        let _ = block!(connection.write(b));
+    }
+    delay.delay_ms(250u8);
+    let message = servo.ram_write(WritableRamAddr::AckPolicy(2));
+    for b in message {
+        let _ = block!(connection.write(b));
+    }
+}
+
+fn init_eth<E: core::fmt::Debug>(eth: &mut W5500, spi: &mut FullDuplex<u8, Error = E>) {
+    //eth.set_mode(spi,false, false, false, true).unwrap();
+    // using a 'locally administered' MAC address
+    eth.init(spi).expect("Failed to initialize w5500");
+    eth.set_mode(spi, false, false, false, true).unwrap();
+    eth.set_mac(spi, &MacAddress::new(0x02, 0x01, 0x02, 0x03, 0x04, 0x05))
+        .unwrap();
+    eth.set_ip(spi, &IpAddress::new(192, 168, 0, 222)).unwrap();
+    eth.set_subnet(spi, &IpAddress::new(255, 255, 255, 0))
+        .unwrap();
+    eth.set_gateway(spi, &IpAddress::new(192, 168, 0, 254))
+        .unwrap();
+    eth.reset_interrupt(spi, SOCKET_UDP, Interrupt::Received)
+        .expect("Failed ot reset interrupts for W5500");
+    eth.listen_udp(spi, SOCKET_UDP, 51)
+        .expect("Faild to listen to port 51");
+}
+
+fn main() -> ! {
+    let chip = Peripherals::take().unwrap();
+    let cortex = CortexPeripherals::take().unwrap();
+
+    let mut robot = init_peripherals(chip, cortex);
+    let mut eth = W5500::new(&mut robot.spi_eth, &mut robot.pb8);
+    init_eth(&mut eth, &mut robot.spi_eth);
+    init_servos(&mut robot.servo_tx, &mut robot.delay);
+
+    robot.delay.delay_ms(50u32);
+
+    let mut buffer = [0; 2048];
+
+    loop {
+        if let Some((_, _, size)) = eth
+            .try_receive_udp(&mut robot.spi_eth, SOCKET_UDP, &mut buffer)
+            .unwrap()
+        {
+            if let Ok(kind) = MessageKind::from_u8(buffer[0]) {
+                let mut data = Message::new();
+                for b in &buffer[1..size] {
+                    data.push(*b);
+                }
+                let frame = Frame::new(kind, data);
+                if let Ok(servos) = ServoGroup::from_message(frame.data) {
+                    for servo in servos.servos {
+                        let s = HServo::new(servo.id);
+                        let msg = match servo.control {
+                            Control::Position(pos) => s.set_position(pos),
+                            Control::Speed(speed) => s.set_speed(speed),
+                        };
+                        for b in msg {
+                            block!(robot.servo_tx.write(b)).expect(
+                                "Fail to communicate with \
+                                 servo",
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+//  For any hard faults, show a message on the debug console and stop.
+exception!(HardFault, hard_fault);
+
+fn hard_fault(ef: &ExceptionFrame) -> ! {
+    asm::bkpt();
+    panic!("Hard fault: {:#?}", ef);
+}
+
+//  For any unhandled interrupts, show a message on the debug console and stop.
+exception!(*, default_handler);
+
+fn default_handler(irqn: i16) {
+    asm::bkpt();
+    panic!("Unhandled exception (IRQn = {})", irqn);
+}
---
+++ src/robot.rs
@@ -0,0 +1,89 @@
+use f103::Peripherals;
+use CortexPeripherals;
+
+use f103_hal::delay::Delay;
+use f103_hal::gpio::{gpioa::*, gpiob::*, Alternate, Floating, Input, Output, PushPull};
+use f103_hal::prelude::*;
+use f103_hal::serial::{Rx, Serial, Tx};
+use f103_hal::spi::*;
+
+use f103::{SPI1, USART2};
+
+type SpiPins = (
+    PA5<Alternate<PushPull>>,
+    PA6<Input<Floating>>,
+    PA7<Alternate<PushPull>>,
+);
+
+pub struct Robot<T, K, P> {
+    pub spi_eth: Spi<K, P>,
+    pub servo_tx: Tx<T>,
+    pub servo_rx: Rx<T>,
+    pub delay: Delay,
+    pub pb8: PB8<Output<PushPull>>,
+}
+
+pub fn init_peripherals(
+    chip: Peripherals,
+    cortex: CortexPeripherals,
+) -> Robot<USART2, SPI1, SpiPins> {
+    //  Get the clocks from the STM32 Reset and Clock Control (RCC) and freeze the Flash Access Control Register (ACR).
+    let mut rcc = chip.RCC.constrain();
+    let mut flash = chip.FLASH.constrain();
+    let mut afio = chip.AFIO.constrain(&mut rcc.apb2);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let _channels = chip.DMA1.split(&mut rcc.ahb);
+
+    //  Configuration des GPIOs
+    let mut gpiob = chip.GPIOB.split(&mut rcc.apb2);
+    let mut gpioa = chip.GPIOA.split(&mut rcc.apb2);
+
+    // Configuration des PINS
+
+    let mut pb8 = gpiob.pb8.into_push_pull_output(&mut gpiob.crh);
+    pb8.set_low();
+
+    let sclk = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
+    let miso = gpioa.pa6.into_floating_input(&mut gpioa.crl);
+    let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
+
+    let pa2 = gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl);
+    let pa3 = gpioa.pa3.into_floating_input(&mut gpioa.crl);
+
+    // Configuration des USART
+
+    let spi = Spi::spi1(
+        chip.SPI1,
+        (sclk, miso, mosi),
+        &mut afio.mapr,
+        Mode {
+            polarity: Polarity::IdleLow,
+            phase: Phase::CaptureOnFirstTransition,
+        },
+        1.mhz(),
+        clocks,
+        &mut rcc.apb2,
+    );
+
+    let servo = Serial::usart2(
+        chip.USART2,
+        (pa2, pa3),
+        &mut afio.mapr,
+        115_200.bps(),
+        clocks,
+        &mut rcc.apb1,
+    );
+
+    let (servo_tx, servo_rx) = servo.split();
+
+    //  Create a delay timer from the RCC clocks.
+    let delay = Delay::new(cortex.SYST, clocks);
+
+    Robot {
+        spi_eth: spi,
+        servo_tx,
+        servo_rx,
+        delay,
+        pb8,
+    }
+}
